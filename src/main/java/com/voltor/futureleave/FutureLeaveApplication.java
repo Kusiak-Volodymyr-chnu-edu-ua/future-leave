c9f97f94d6d04ff3f48de7734e97b4fc0d1eb477
Volodymyr Kusiak <ispas.vova@gmail.com>
2023-09-17 20:01:09 +0300

    GH-2 Project creation

---
+++ src/main/java/com/voltor/futureleave/FutureLeaveApplication.java
@@ -0,0 +1,13 @@
+package com.voltor.futureleave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureLeaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutureLeaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/voltor/futureleave/FutureLeaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voltor.futureleave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutureLeaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
